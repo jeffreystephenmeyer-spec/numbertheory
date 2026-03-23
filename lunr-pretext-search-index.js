924fe015499d790a489f7dd88f7cecac8cbb6908
Jeffrey S Meyer <jeffrey.stephen.meyer@gmail.com>
2026-03-23 15:05:28 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,302 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "chp1sec0",
+  "level": "1",
+  "url": "chp1sec0.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "What are the integers?",
+  "body": " What are the integers?  Text of section.   What are integers?  We work with integers every day - but what are they?! Give your definition of integers.   "
+},
+{
+  "id": "chp1sec0-3",
+  "level": "2",
+  "url": "chp1sec0.html#chp1sec0-3",
+  "type": "Activity",
+  "number": "1.1.1",
+  "title": "What are integers?",
+  "body": " What are integers?  We work with integers every day - but what are they?! Give your definition of integers.  "
+},
+{
+  "id": "chp1sec1",
+  "level": "1",
+  "url": "chp1sec1.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "The algebra of the integers",
+  "body": " The algebra of the integers     Commutative Ring with Identity   A commutative ring with identity is a set with two binary operations:   an addition operation and a    multiplication operation    satisfying 6 axioms:   (Addition is associative .) If , then     (Addition has an identity .) If , then     (Addition has inverses .) If , then there exists so that     (Addition is commutative .) If , then     (Multiplication is associative .) If , then     (Multiplication has an identity .) If , then         s   If is a commutative ring with identity, and  .    ggg     s   If is a commutative ring with identity, and  .    ggg    "
+},
+{
+  "id": "def-definition-commutative-ring",
+  "level": "2",
+  "url": "chp1sec1.html#def-definition-commutative-ring",
+  "type": "Definition",
+  "number": "1.2.1",
+  "title": "Commutative Ring with Identity.",
+  "body": " Commutative Ring with Identity   A commutative ring with identity is a set with two binary operations:   an addition operation and a    multiplication operation    satisfying 6 axioms:   (Addition is associative .) If , then     (Addition has an identity .) If , then     (Addition has inverses .) If , then there exists so that     (Addition is commutative .) If , then     (Multiplication is associative .) If , then     (Multiplication has an identity .) If , then       "
+},
+{
+  "id": "prop-1_1-01",
+  "level": "2",
+  "url": "chp1sec1.html#prop-1_1-01",
+  "type": "Proposition",
+  "number": "1.2.2",
+  "title": "s.",
+  "body": " s   If is a commutative ring with identity, and  .    ggg   "
+},
+{
+  "id": "prop-1_1-02",
+  "level": "2",
+  "url": "chp1sec1.html#prop-1_1-02",
+  "type": "Proposition",
+  "number": "1.2.3",
+  "title": "s.",
+  "body": " s   If is a commutative ring with identity, and  .    ggg   "
+},
+{
+  "id": "chp1sec2",
+  "level": "1",
+  "url": "chp1sec2.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "The order of the integers",
+  "body": " The order of the integers     Order   A set has a (total) order if there is a binary relationship satisfying:   ( reflexivity .) If , then     ( transitivity .) If satisfy and , then     ( symmetry .) If satisfy and , then     ( totality .) If , then one of the following hold: or         Ordered Ring   A ring is an ordered ring if has an order that is compatible with its operations in the following satisfying:   ( translation .) If and , then     ( scaling .) If satisfy and , then        Observe the symbols , , , and will all be used in the usual ways.  We call elements of an ordered ring positive if it is greater than zero and negative if it is less than zero.     If is an ordered ring and is positive (resp. negative), then is negative (resp. positive).    ggg     Well-Ordered Set   If is a set with a total order, it is well-ordered if every subset contains a minimal element , i.e. in symbols, if then there exists an element such that, for all it is the case that .       If is a ordered ring whose positive elements are well-ordered, then the minimal positive element, is invertible.    ggg       If is a ordered ring whose positive elements are well-ordered, then the minimal positive element is 1.    ggg       If is a ordered ring whose positive elements are well-ordered, and f satisfy and , then .    ggg     Well-Ordered Rings are Domains   If is a ordered ring whose positive elements are well-ordered, and satisfy , then either or .    ggg     Well-Ordered Rings Have Cancellation   If is a ordered ring whose positive elements are well-ordered, and satisfy , then either or .    ggg    The integers, are in fact the unique ordered commutative ring with identity whose positive elements are well-ordered. Going forward we will simply refer to this set as the integers and all properties of the integers will follow from these.  "
+},
+{
+  "id": "def-definition-order",
+  "level": "2",
+  "url": "chp1sec2.html#def-definition-order",
+  "type": "Definition",
+  "number": "1.3.1",
+  "title": "Order.",
+  "body": " Order   A set has a (total) order if there is a binary relationship satisfying:   ( reflexivity .) If , then     ( transitivity .) If satisfy and , then     ( symmetry .) If satisfy and , then     ( totality .) If , then one of the following hold: or       "
+},
+{
+  "id": "def-definition-ordered-ring",
+  "level": "2",
+  "url": "chp1sec2.html#def-definition-ordered-ring",
+  "type": "Definition",
+  "number": "1.3.2",
+  "title": "Ordered Ring.",
+  "body": " Ordered Ring   A ring is an ordered ring if has an order that is compatible with its operations in the following satisfying:   ( translation .) If and , then     ( scaling .) If satisfy and , then       "
+},
+{
+  "id": "chp1sec2-6",
+  "level": "2",
+  "url": "chp1sec2.html#chp1sec2-6",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "positive negative "
+},
+{
+  "id": "prop-01",
+  "level": "2",
+  "url": "chp1sec2.html#prop-01",
+  "type": "Proposition",
+  "number": "1.3.3",
+  "title": ".",
+  "body": "   If is an ordered ring and is positive (resp. negative), then is negative (resp. positive).    ggg   "
+},
+{
+  "id": "def-definition-well-ordered",
+  "level": "2",
+  "url": "chp1sec2.html#def-definition-well-ordered",
+  "type": "Definition",
+  "number": "1.3.4",
+  "title": "Well-Ordered Set.",
+  "body": " Well-Ordered Set   If is a set with a total order, it is well-ordered if every subset contains a minimal element , i.e. in symbols, if then there exists an element such that, for all it is the case that .   "
+},
+{
+  "id": "prop-02",
+  "level": "2",
+  "url": "chp1sec2.html#prop-02",
+  "type": "Proposition",
+  "number": "1.3.5",
+  "title": ".",
+  "body": "   If is a ordered ring whose positive elements are well-ordered, then the minimal positive element, is invertible.    ggg   "
+},
+{
+  "id": "prop-03",
+  "level": "2",
+  "url": "chp1sec2.html#prop-03",
+  "type": "Proposition",
+  "number": "1.3.6",
+  "title": ".",
+  "body": "   If is a ordered ring whose positive elements are well-ordered, then the minimal positive element is 1.    ggg   "
+},
+{
+  "id": "prop-04",
+  "level": "2",
+  "url": "chp1sec2.html#prop-04",
+  "type": "Proposition",
+  "number": "1.3.7",
+  "title": ".",
+  "body": "   If is a ordered ring whose positive elements are well-ordered, and f satisfy and , then .    ggg   "
+},
+{
+  "id": "prop-05",
+  "level": "2",
+  "url": "chp1sec2.html#prop-05",
+  "type": "Proposition",
+  "number": "1.3.8",
+  "title": "Well-Ordered Rings are Domains.",
+  "body": " Well-Ordered Rings are Domains   If is a ordered ring whose positive elements are well-ordered, and satisfy , then either or .    ggg   "
+},
+{
+  "id": "prop-06",
+  "level": "2",
+  "url": "chp1sec2.html#prop-06",
+  "type": "Proposition",
+  "number": "1.3.9",
+  "title": "Well-Ordered Rings Have Cancellation.",
+  "body": " Well-Ordered Rings Have Cancellation   If is a ordered ring whose positive elements are well-ordered, and satisfy , then either or .    ggg   "
+},
+{
+  "id": "chp2sec1",
+  "level": "1",
+  "url": "chp2sec1.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "The Division Algorithm",
+  "body": " The Division Algorithm     Divisibility   An integer is divisible by an integer if there exists an integer so that .    If is divisible by , we say also say  divides  . We symbolize this by . If does not divide , then we write . We say that is a divisor of .     If where , then                     ggg     Divisibility is transitive   If where and , then .    ggg       If where and , then for any  .    ggg     The Division Algorithm   If where , then there exists unique integers satisfying    and     .       ggg    "
+},
+{
+  "id": "def-definition-divisibility",
+  "level": "2",
+  "url": "chp2sec1.html#def-definition-divisibility",
+  "type": "Definition",
+  "number": "2.1.1",
+  "title": "Divisibility.",
+  "body": " Divisibility   An integer is divisible by an integer if there exists an integer so that .   "
+},
+{
+  "id": "chp2sec1-4",
+  "level": "2",
+  "url": "chp2sec1.html#chp2sec1-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "divides divisor "
+},
+{
+  "id": "prop-2-0",
+  "level": "2",
+  "url": "chp2sec1.html#prop-2-0",
+  "type": "Proposition",
+  "number": "2.1.2",
+  "title": ".",
+  "body": "   If where , then                     ggg   "
+},
+{
+  "id": "prop-2-1",
+  "level": "2",
+  "url": "chp2sec1.html#prop-2-1",
+  "type": "Proposition",
+  "number": "2.1.3",
+  "title": "Divisibility is transitive.",
+  "body": " Divisibility is transitive   If where and , then .    ggg   "
+},
+{
+  "id": "prop-2-2",
+  "level": "2",
+  "url": "chp2sec1.html#prop-2-2",
+  "type": "Proposition",
+  "number": "2.1.4",
+  "title": ".",
+  "body": "   If where and , then for any  .    ggg   "
+},
+{
+  "id": "prop-2-3",
+  "level": "2",
+  "url": "chp2sec1.html#prop-2-3",
+  "type": "Proposition",
+  "number": "2.1.5",
+  "title": "The Division Algorithm.",
+  "body": " The Division Algorithm   If where , then there exists unique integers satisfying    and     .       ggg   "
+},
+{
+  "id": "chp2sec2",
+  "level": "1",
+  "url": "chp2sec2.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "GCD",
+  "body": " GCD     Common Divisor   If and are integers and there is an integer that divides both of them, then is a common divisor of and .     Greatest Common Divisor (GCD)   If and are integers, if is a common divisor of and such that if is any other common divisor of and , then , then is the greatest common divisor (gcd) of and .    We denote the gcd of integers and by .     If where and , then .    ggg     The Euclidean Algorithm   If where , then    ggg     Bezout Identity   If then there exists so that .    ggg    "
+},
+{
+  "id": "def-definition-common_divisor",
+  "level": "2",
+  "url": "chp2sec2.html#def-definition-common_divisor",
+  "type": "Definition",
+  "number": "2.2.1",
+  "title": "Common Divisor.",
+  "body": " Common Divisor   If and are integers and there is an integer that divides both of them, then is a common divisor of and .   "
+},
+{
+  "id": "def-definition-gcd",
+  "level": "2",
+  "url": "chp2sec2.html#def-definition-gcd",
+  "type": "Definition",
+  "number": "2.2.2",
+  "title": "Greatest Common Divisor (GCD).",
+  "body": " Greatest Common Divisor (GCD)   If and are integers, if is a common divisor of and such that if is any other common divisor of and , then , then is the greatest common divisor (gcd) of and .   "
+},
+{
+  "id": "prop-2_2-1",
+  "level": "2",
+  "url": "chp2sec2.html#prop-2_2-1",
+  "type": "Proposition",
+  "number": "2.2.3",
+  "title": ".",
+  "body": "   If where and , then .    ggg   "
+},
+{
+  "id": "prop-2_2-2",
+  "level": "2",
+  "url": "chp2sec2.html#prop-2_2-2",
+  "type": "Proposition",
+  "number": "2.2.4",
+  "title": "The Euclidean Algorithm.",
+  "body": " The Euclidean Algorithm   If where , then    ggg   "
+},
+{
+  "id": "prop-2_2-3",
+  "level": "2",
+  "url": "chp2sec2.html#prop-2_2-3",
+  "type": "Proposition",
+  "number": "2.2.5",
+  "title": "Bezout Identity.",
+  "body": " Bezout Identity   If then there exists so that .    ggg   "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
